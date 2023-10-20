9f3f742d2da7cbdc0de5b001d0239cc8865d0756
lochanikalidasu <k8978899033@gmail.com>
2023-10-20 23:28:33 +0530

    changes made for the profile page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:expense_tracker_app/splash.screen.dart';
 import 'package:flutter/material.dart';
 import 'constants.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
